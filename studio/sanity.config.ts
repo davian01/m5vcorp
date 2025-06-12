33e53fac3b3891ddea49a397d100673b733f9d8c
davian01 <davian.brooks01@gmail.com>
2025-06-11 22:00:45 -0400

    removed sanity

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,13 +1,1 @@
-import { defineConfig } from 'sanity'
-
-export default defineConfig({
-  name: 'm5v-corp',
-  title: 'm5v-corp',
-  projectId: 'eddugmcj',
-  dataset: 'production',
-
-  // no imports, no plugins, no schemaTypes at all
-  schema: {
-    types: []
-  }
-})
+//empty config
